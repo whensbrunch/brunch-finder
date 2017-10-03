eeb4c3747c2105fabec45e0f9a6448dc01dd2c15
whensbrunch <whensbrunch@gmail.com>
2017-10-03 19:15:26 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/new'
+
+  get '/signup', to: 'users#new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
