c8b164653bbc95aa054d832772e3d64257c76769
whensbrunch <whensbrunch@gmail.com>
2017-10-07 01:03:31 -0300

    db migrate working for tags

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   geocoded_by :address
+  has_many :tags, through: :taggings
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, presence: true, length: { maximum: 255 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003233350) do
+ActiveRecord::Schema.define(version: 20171007035125) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
