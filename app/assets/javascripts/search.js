cb8bc8df83d27de0a930d1ffc00e42ee3edc32cc
whensbrunch <whensbrunch@gmail.com>
2017-10-31 23:48:37 -0300

    add description of address under restaurant name

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,21 +3,34 @@ $( document ).ready(function() {
   $input = $("[data-behavior='autocomplete']");
 
   var options = {
-    getValue: "name",
+
     url: function(phrase) {
       return "/search.json?q=" + phrase;
     },
+
+    getValue: "name",
+
+    template: {
+         type: "description",
+         fields: {
+             description: "address"
+         }
+     },
+
     categories: [
       {
         listLocation: "restaurants",
-        header: "<strong>Restaurants</strong>"
+        // header: "<strong>Restaurants</strong>"
       }
     ],
+
     list: {
       onChooseEvent: function() {
+        // get id of user selection
         var id = $input.getSelectedItemData().id
         console.log(id)
 
+        // place it in hidden field
         var hiddenField = document.getElementById('review_restaurant_id');
         hiddenField.value = id;
       }
